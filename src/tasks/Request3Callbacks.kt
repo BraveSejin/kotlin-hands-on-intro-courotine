62e7cbeba146d9692d320897139677d2caa149c4
BraveSejin <ihavebravebiz@gmail.com>
2022-07-18 22:06:12 +0900

    callback sol 1: order of api call result not guranteed

--- src/tasks/Request3Callbacks.kt
+++ src/tasks/Request3Callbacks.kt
@@ -12,15 +12,18 @@ fun loadContributorsCallbacks(service: GitHubService, req: RequestData, updateRe
         logRepos(req, responseRepos)
         val repos = responseRepos.bodyList()
         val allUsers = mutableListOf<User>()
-        for (repo in repos) {
+        for ((index,repo) in repos.withIndex()) {
             service.getRepoContributorsCall(req.org, repo.name).onResponse { responseUsers ->
                 logUsers(repo, responseUsers)
                 val users = responseUsers.bodyList()
                 allUsers += users
+                if (index == repos.lastIndex){
+                    updateResults(allUsers.aggregate())
+                }
             }
         }
         // TODO: Why this code doesn't work? How to fix that?
-        updateResults(allUsers.aggregate())
+
     }
 }
 
