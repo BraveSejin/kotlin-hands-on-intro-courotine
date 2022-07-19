fc9f09ba27e956dce5576ec179d28c0e8e50261a
BraveSejin <ihavebravebiz@gmail.com>
2022-07-19 13:44:40 +0900

    coroutine cacellation and scope

--- src/tasks/Request5NotCancellable.kt
+++ src/tasks/Request5NotCancellable.kt
@@ -5,5 +5,25 @@ import kotlinx.coroutines.*
 import kotlin.coroutines.coroutineContext
 
 suspend fun loadContributorsNotCancellable(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    val repos =
+        service
+            .getOrgReposCall(req.org)
+            .also { logRepos(req, it) }
+            .body() ?: listOf()
+
+    lateinit var result : List<User>
+    val deffereds = repos.map { repo ->
+
+        GlobalScope.async {
+            log("starting loading for ${repo.name}")
+            delay(3000)
+            service.getRepoContributorsCall(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }
+    }
+
+    result = deffereds.awaitAll().flatten().aggregate()
+    return result
+
 }
